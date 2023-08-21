c6201693c82b7ab139641bc1aba289a32ecb76bb
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-08-22 01:16:48 +0200

    remove console.log

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,6 @@
 import $ from "jquery";
 
 $(() => {
-    console.log('It works');
     $("p").add("<p>Holberton Dashboard</p>").appendTo(document.body);
     $("p").add("<p>Dashboard data for the students</p>").appendTo(document.body);
     $("p").add("<p>Copyright - Holberton School</p>").appendTo(document.body);
