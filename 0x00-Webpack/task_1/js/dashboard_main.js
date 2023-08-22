152c54c8ee9cba3ea86a6ebe8e164cb48ca1e63a
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-08-22 02:07:52 +0200

    using jquery with lodash and webpack

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,19 @@
+import $ from "jquery";
+const _ = require('lodash');
+
+$(() => {
+    $("body").append("<p>Holberton Dashboard</p>");
+    $("body").append("<p>Dashboard data for the students</p>");
+    $("body").append("<button>Click here to get started</button>");
+    $("body").append("<p id='count'></p>");
+    $("body").append("<p>Copyright - Holberton School</p>");
+
+    let count = 0;
+    function updateCounter() {
+        count = count + 1;
+        const para = document.getElementById('count');
+        para.innerHTML = `${count} clicks on the button`;
+    }
+
+    _.debounce($("button").on("click", updateCounter));
+});
