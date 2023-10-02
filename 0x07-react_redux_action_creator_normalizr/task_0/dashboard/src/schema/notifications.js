f54992bd03139785116835b48860659f76209721
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-10-02 17:46:59 +0200

    redux

---
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -0,0 +1,7 @@
+import * as notifications from "../../notifications.json";
+
+export function getAllNotificationsByUser(userId){
+    return notifications.default.filter(
+        notification => notification.author.id === userId
+        ).map(item => item.context);
+}
